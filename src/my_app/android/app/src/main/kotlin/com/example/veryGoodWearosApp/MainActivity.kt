34c26253e822569c879ecc93e639950186512252
Renan Araujo <renanmatheuscorreiaaraujo@gmail.com>
2023-03-14 14:57:13 +0000

    docs: add readmes and docs

--- src/my_app/android/app/src/main/kotlin/com/example/veryGoodWearosApp/MainActivity.kt
+++ src/my_app/android/app/src/main/kotlin/com/example/veryGoodWearosApp/MainActivity.kt
@@ -1,11 +1,18 @@
+// Dear manitainer, if you have to change this file,
+// consider changing the one under `tool/generator/static/` too.
 package com.example.veryGoodWearosApp
 
 import android.os.Bundle
 import androidx.annotation.NonNull
+import com.samsung.wearable_rotary.WearableRotaryPlugin
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+
+    /**
+     * A method to hook rotary input events into the "WearableRotaryPlugin" class.
+     */
     override fun onGenericMotionEvent(event: MotionEvent?): Boolean {
         return when {
             WearableRotaryPlugin.onGenericMotionEvent(event) -> true
@@ -13,6 +20,9 @@ class MainActivity: FlutterActivity() {
         }
     }
 
+    /**
+     * Makes the app assume the rounded canvas appearance on rounded screens.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         intent.putExtra("background_mode", "transparent")
         super.onCreate(savedInstanceState)
--- tool/generator/static/MainActivity.kt
+++ tool/generator/static/MainActivity.kt
@@ -2,10 +2,15 @@ package {{org_name.dotCase()}}.{{project_name.snakeCase()}}
 
 import android.os.Bundle
 import androidx.annotation.NonNull
+import com.samsung.wearable_rotary.WearableRotaryPlugin
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+
+    /**
+     * A method to hook rotary input events into the "WearableRotaryPlugin" class.
+     */
     override fun onGenericMotionEvent(event: MotionEvent?): Boolean {
         return when {
             WearableRotaryPlugin.onGenericMotionEvent(event) -> true
@@ -13,6 +18,9 @@ class MainActivity: FlutterActivity() {
         }
     }
 
+    /**
+     * Makes the app assume the rounded canvas appearance on rounded screens.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         intent.putExtra("background_mode", "transparent")
         super.onCreate(savedInstanceState)
